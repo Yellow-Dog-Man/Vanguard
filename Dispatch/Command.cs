5c3de62ab5f37d9427598936f3df900b270ea682
Schuyler Cebulskie <Gawdl3y@gawdl3y.com>
2025-07-30 23:41:09 -0400

    feat#: Add Command members useful for dispatch

--- Dispatch/Command.cs
+++ Dispatch/Command.cs
@@ -35,6 +35,16 @@ public class Command
 	public IReadOnlyList<Argument> Arguments { get; init; }
 
 	/// <summary>
+	/// Gets the number of arguments that are optional
+	/// </summary>
+	public int OptionalArgumentCount => Arguments.Where(a => a.IsOptional).Count();
+
+	/// <summary>
+	/// Gets the range of argument counts the command can accept
+	/// </summary>
+	public (int, int) ValidArgumentCount => (Arguments.Count - OptionalArgumentCount, Arguments.Count);
+
+	/// <summary>
 	/// Creates a new Command using reflection to obtain all information for it from a given controller method
 	/// </summary>
 	/// <param name="method"></param>
@@ -67,5 +77,59 @@ public class Command
 		Name = commandAttr?.Name ?? method.Name;
 		Aliases = commandAttr?.Aliases?.ToFrozenSet(StringComparer.OrdinalIgnoreCase) ?? FrozenSet<string>.Empty;
 		Arguments = method.GetParameters().Select(Argument.Create).ToImmutableArray();
+	}
+
+	/// <summary>
+	/// Gets the arguments to use for a provided number of values
+	/// </summary>
+	/// <param name="valCount"></param>
+	/// <returns></returns>
+	/// <exception cref="ArgumentCountException">When <c>valCount</c> is out of range of <see cref="ValidArgumentCount"/></exception>
+	public IEnumerable<Argument> GetArgumentsToUse(int valCount)
+	{
+		var (minArgs, maxArgs) = ValidArgumentCount;
+		if (valCount < minArgs || valCount > maxArgs)
+			throw new ArgumentCountException(this, valCount);
+
+		return Arguments.Take(valCount);
+	}
+
+	/// <summary>
+	/// Indicates that not enough or too many values were supplied to a command's execution
+	/// </summary>
+	/// <param name="command"></param>
+	/// <param name="valCount"></param>
+	public class ArgumentCountException(Command command, int valCount)
+		: ArgumentOutOfRangeException(BuildMessage(command, valCount))
+	{
+		/// <summary>
+		/// Command that the arguments were being supplied to
+		/// </summary>
+		public Command Command { get; init; } = command;
+
+		/// <summary>
+		/// Number of arguments that were supplied
+		/// </summary>
+		public int SuppliedValueCount = valCount;
+
+		/// <summary>
+		/// Builds a message for the exception
+		/// </summary>
+		/// <param name="command"></param>
+		/// <param name="valCount"></param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentOutOfRangeException">When <c>valCount</c> is within <see cref="Command.ValidArgumentCount"/></exception>
+		private static string BuildMessage(Command command, int valCount)
+		{
+			var (minArgs, maxArgs) = command.ValidArgumentCount;
+
+			if (valCount < minArgs)
+				return $"Not enough values provided to the {command.Name} command; expected between {minArgs} and {maxArgs}, but got {valCount}";
+
+			if (valCount > maxArgs)
+				return $"Too many values provided to the {command.Name} command; expected between {minArgs} and {maxArgs}, but got {valCount}";
+
+			throw new ArgumentOutOfRangeException($"Argument count is within the {command.Name} command's expected range");
+		}
 	}
 }
